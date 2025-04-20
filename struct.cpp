e850074d7c2650c22aa68d7bbf874cc97dc8e5ca
riswandaP <riswandaputra03@gmail.com>
2025-04-20 15:11:05 +0700

    InputFungsiMAinStruct

--- struct.cpp
+++ struct.cpp
@@ -8,3 +8,23 @@ struct Mahasiswa
     string alamat;
 };
 
+int main()
+{
+    // Membuat object struct Mahasiswa
+    Mahasiswa mhs;
+
+    cout << "Masukan nim = ";
+    cin >> mhs.nim;
+    cout << "Masukan nama = ";
+    cin >> mhs.nama;
+    cout << "Masukan alamat = ";
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+}
