31275b8beee8c0235ba65e5dbd6ea3bd492d65fd
riswandaP <riswandaputra03@gmail.com>
2025-04-20 15:08:18 +0700

    FungsiStructGetline

--- struct_getline.cpp
+++ struct_getline.cpp
@@ -2,3 +2,16 @@
 #include <string>
 using namespace std;
 
+struct DetailAlamat
+{
+    string kota;
+    string provinsi;
+};
+
+struct Mahasiswa
+{
+    string nama;
+    string nim;
+    DetailAlamat alamat;
+};
+
